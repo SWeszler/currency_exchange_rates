f41c7812b3b45d52e68ea291ea4da563c01ad3dd
Sebastian Weszler <s.weszler@gmail.com>
2021-11-14 19:56:35 +0800

    remove hard-coded default django secret_key

--- exchange_rates/settings.py
+++ exchange_rates/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2f!&29&s6won&*@pb_wbe@&^!k%9)-xd(7ygia7@)o7500q(st'
+SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
